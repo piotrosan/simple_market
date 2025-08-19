9190b922046024bf00040fa4eeeea8270ddc130f
piotr <piotr.kolodziejski@gmail.com>
2025-08-19 09:28:48 +0000

    change goods model and add order

--- goods/models.py
+++ goods/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 from app_support_place.generic_class.mixins import CreatedUpdatedMixin
 
+class GoodsBrand(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    logo = models.ImageField(upload_to='brands/', null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
 
 class GoodsCategory(
     models.Model,
@@ -23,6 +32,7 @@ class Goods(
         null=False,
         blank=False
     )
+    slug = models.SlugField(unique=True)
     goods_parameter = models.JSONField()
     credit = models.DecimalField(
         max_digits=6,
